dde2ef4fb42843a9d36157602f66821fcee7e2d8
SuMuzi <38148543+SuMuzi@users.noreply.github.com>
2024-02-25 16:45:08 +0800

    Update block.py

--- blocks/block.py
+++ blocks/block.py
@@ -84,5 +84,63 @@ class up_conv(nn.Module):
     def forward(self, x):
         x = self.up(x)
         return x
+class ResidualInResidualDenseNetwork(nn.Module):
+    def __init__(self, nf,gc,n_basic_block=23):
+        super(ResidualInResidualDenseNetwork, self).__init__()
+        # for _ in range(n_basic_block):
+
+        if block_num == 23:
+            self.layer1 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer2 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer3 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer4 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer5 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer6 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer7 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer8 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer9 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer10 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer11 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer12 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer13 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer14 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer15 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer16 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer17 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer18 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer19 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer20 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer21 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer22 = ResidualInResidualDenseBlock(nf, gc)
+            self.layer23 = ResidualInResidualDenseBlock(nf, gc)
+
+    def forward(self, x):
+        out_1 = self.layer1(x)
+        out_2 = self.layer2(out_1)
+        out_3 = self.layer3(out_2)
+        out_4 = self.layer4(out_3)
+        out_5 = self.layer5(out_4)
+        out_6 = self.layer6(out_5)
+        out_7 = self.layer7(out_6)
+        out_8 = self.layer8(out_7)
+        out_9 = self.layer9(out_8)
+        out_10 = self.layer10(out_9)
+        out_11 = self.layer11(out_10)
+        out_12 = self.layer12(out_11)
+        out_13 = self.layer13(out_12)
+        out_14 = self.layer14(out_13)
+        out_15 = self.layer15(out_14)
+        out_16 = self.layer16(out_15)
+        out_17 = self.layer17(out_16)
+        out_18 = self.layer18(out_17)
+        out_19 = self.layer19(out_18)
+        out_20 = self.layer20(out_19)
+        out_21 = self.layer21(out_20)
+        out_22 = self.layer22(out_21)
+        out_23 = self.layer23(out_22)
+
+        return torch.cat((out_1,out_2,out_3,out_4,out_5,out_6,out_7,out_8,out_9,
+                          out_10,out_11,out_12,out_13,out_14,out_15,out_16,out_17,
+                          out_18,out_19,out_20,out_21,out_22,out_23),dim=1)
 
 
