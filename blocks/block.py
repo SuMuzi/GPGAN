82315a429cd6e4f9ea868002ae423695a403539d
SuMuzi <38148543+SuMuzi@users.noreply.github.com>
2024-02-25 16:50:51 +0800

    Update block.py

--- blocks/block.py
+++ blocks/block.py
@@ -5,11 +5,11 @@ import torch
 class ResidualDenseBlock(nn.Module):
     def __init__(self, nf, gc=32, res_scale=0.2):
         super(ResidualDenseBlock, self).__init__()
-        self.layer1 = nn.Sequential(nn.Conv2d(nf + 0 * gc, gc, (3,3), padding=1, bias=False), nn.LeakyReLU())
-        self.layer2 = nn.Sequential(nn.Conv2d(nf + 1 * gc, gc, (3,3), padding=1, bias=False), nn.LeakyReLU())
-        self.layer3 = nn.Sequential(nn.Conv2d(nf + 2 * gc, gc, (3,3), padding=1, bias=False), nn.LeakyReLU())
-        self.layer4 = nn.Sequential(nn.Conv2d(nf + 3 * gc, gc, (3,3), padding=1, bias=False), nn.LeakyReLU())
-        self.layer5 = nn.Sequential(nn.Conv2d(nf + 4 * gc, nf, (3,3), padding=1, bias=False), nn.LeakyReLU())
+        self.layer1 = nn.Sequential(nn.Conv2d(nf + 0 * gc, gc, (3,3), padding=1, bias=False), nn.ReLU())
+        self.layer2 = nn.Sequential(nn.Conv2d(nf + 1 * gc, gc, (3,3), padding=1, bias=False), nn.ReLU())
+        self.layer3 = nn.Sequential(nn.Conv2d(nf + 2 * gc, gc, (3,3), padding=1, bias=False), nn.ReLU())
+        self.layer4 = nn.Sequential(nn.Conv2d(nf + 3 * gc, gc, (3,3), padding=1, bias=False), nn.ReLU())
+        self.layer5 = nn.Sequential(nn.Conv2d(nf + 4 * gc, nf, (3,3), padding=1, bias=False), nn.ReLU())
 
         self.res_scale = res_scale
 
@@ -43,7 +43,7 @@ def upsample_block(nf, scale_factor=3):
         block += [
             nn.Conv2d(nf, nf * (scale_factor ** 2), 1, bias=False),
             nn.PixelShuffle(scale_factor),
-            nn.LeakyReLU()
+            nn.ReLU()
         ]
     return nn.Sequential(*block)
 
@@ -59,10 +59,10 @@ class conv_block(nn.Module):
         self.conv = nn.Sequential(
             nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1, bias=False),
             # nn.BatchNorm2d(out_ch),
-            nn.LeakyReLU(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(out_ch, out_ch, kernel_size=3, stride=1, padding=1, bias=False),
             # nn.BatchNorm2d(out_ch),
-            nn.LeakyReLU(inplace=True))
+            nn.ReLU(inplace=True))
 
     def forward(self, x):
         x = self.conv(x)
